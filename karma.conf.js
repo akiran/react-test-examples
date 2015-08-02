0a22fb676244383ac705fbc3eeda3c9118657a9c
kiran <kiran@webrafter.com>
2015-08-02 09:37:36 +0530

    working

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,12 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'test/**/*.jsx': ['webpack']
+        'test/**/*.jsx': ['webpack' ,'coverage'],
+    },
+
+    coverageReporter: {
+        type: 'html',
+        dir: 'coverage/'
     },
 
     webpack: {
@@ -54,7 +59,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
